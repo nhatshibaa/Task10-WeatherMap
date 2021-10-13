331b5a5c5e6cbe6a5ea63a739df72eb330752c07
nhatshibaa <nhatntth2008069@fpt.edu.vn>
2021-10-13 16:34:28 +0700

    Add project files.

---
+++ Task10-WeatherMap/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Task10_WeatherMap
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        //private async void Btn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var position = await LocationManager.GetPosition();
+        //    Root myWeather = await OpenWeatherMapProxy.GetWeather(position.Coordinate.Latitude, position.Coordinate.Longitude);
+        //    string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
+        //    ResultImg.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+        //    ResultWeather.Text = myWeather.name + " - " + ((int)myWeather.main.temp).ToString() + " - " + myWeather.weather[0].description;
+        //}
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var position = await LocationManager.GetPosition();
+
+                var lat = position.Coordinate.Latitude;
+                var lon = position.Coordinate.Longitude;
+
+                Root myWeather =
+                    await OpenWeatherMapProxy.GetWeather(
+                        lat,
+                        lon);
+
+                // Schedule update
+                var uri = String.Format("http://uwpweatherservice.azurewebsites.net/?lat={0}&lon={1}", lat, lon);
+
+                var tileContent = new Uri(uri);
+                var requestedInterval = PeriodicUpdateRecurrence.HalfHour;
+
+                var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+                updater.StartPeriodicUpdate(tileContent, requestedInterval);
+
+                string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
+                ResultImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+
+                TempTextBlock.Text = ((int)myWeather.main.temp).ToString();
+                DescriptionTextBlock.Text = myWeather.weather[0].description;
+                LocationTextBlock.Text = myWeather.name;
+            }
+            catch
+            {
+                LocationTextBlock.Text = "Unable to get weather at this time.";
+            }
+
+        }
+    }
+}
---
+++ Task10-WeatherMap/OpenWeatherMapProxy.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Task10_WeatherMap
+{
+    public class OpenWeatherMapProxy
+    {
+        public async static Task<Root> GetWeather(double lat, double lon)
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync("https://api.openweathermap.org/data/2.5/weather?lat=21.028511&lon=105.804817&appid=780d5b4b35022d0e3832415b5b38e072&units=imperial");
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(Root));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (Root)serializer.ReadObject(ms);
+
+            return data;
+        }
+    }
+    [DataContract]
+    public class Coord
+    {
+        [DataMember]
+        public double lon { get; set; }
+
+        [DataMember]
+        public double lat { get; set; }
+    }
+    [DataContract]
+    public class Weather
+    {
+        [DataMember]
+        public int id { get; set; }
+
+        [DataMember]
+        public string main { get; set; }
+
+        [DataMember]
+        public string description { get; set; }
+
+        [DataMember]
+        public string icon { get; set; }
+    }
+    [DataContract]
+    public class Main
+    {
+        [DataMember]
+        public double temp { get; set; }
+
+        [DataMember]
+        public double feels_like { get; set; }
+
+        [DataMember]
+        public double temp_min { get; set; }
+
+        [DataMember]
+        public double temp_max { get; set; }
+
+        [DataMember]
+        public int pressure { get; set; }
+
+        [DataMember]
+        public int humidity { get; set; }
+
+        [DataMember]
+        public int sea_level { get; set; }
+
+        [DataMember]
+        public int grnd_level { get; set; }
+    }
+    [DataContract]
+    public class Wind
+    {
+        [DataMember]
+        public double speed { get; set; }
+
+        [DataMember]
+        public int deg { get; set; }
+
+        [DataMember]
+        public double gust { get; set; }
+    }
+    [DataContract]
+    public class Rain
+    {
+        [DataMember]
+        public double _1h { get; set; }
+    }
+    [DataContract]
+    public class Clouds
+    {
+        [DataMember]
+        public int all { get; set; }
+    }
+    //[DataContract]
+    //public class Sys
+    //{
+    //    [DataMember]
+    //    public int type { get; set; }
+    //    public int id { get; set; }
+    //    public string country { get; set; }
+    //    public int sunrise { get; set; }
+    //    public int sunset { get; set; }
+    //}
+    [DataContract]
+    public class Root
+    {
+        [DataMember]
+        public Coord coord { get; set; }
+
+        [DataMember]
+        public List<Weather> weather { get; set; }
+
+        [DataMember]
+        public string @base { get; set; }
+
+        [DataMember]
+        public Main main { get; set; }
+
+        [DataMember]
+        public int visibility { get; set; }
+
+        [DataMember]
+        public Wind wind { get; set; }
+
+        [DataMember]
+        public Rain rain { get; set; }
+
+        [DataMember]
+        public Clouds clouds { get; set; }
+
+        [DataMember]
+        public int dt { get; set; }
+
+        //public Sys sys { get; set; }
+
+        [DataMember]
+        public int timezone { get; set; }
+
+        [DataMember]
+        public int id { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+
+        [DataMember]
+        public int cod { get; set; }
+    }
+}
